6d2768883cddd00c1aa680a4390321ef3afc92d8
Luke Duggan <48234476+devluked@users.noreply.github.com>
2022-07-26 18:43:33 -0700

    Complete issue #2 - Add 404 page for /show/{show_name} pages that don't exist

---
+++ pages/404.js
@@ -0,0 +1,12 @@
+import Layout from "@c/Layout";
+
+const NotFound = () => {
+  return (
+    <Layout>
+      <h1>404</h1>
+      <p>Nothing to see here!</p>
+    </Layout>
+  );
+};
+
+export default NotFound;
--- pages/show/[slug].js
+++ pages/show/[slug].js
@@ -1,10 +1,10 @@
-import ReactMarkdown from 'react-markdown'
-import styled from 'styled-components'
-import Layout from '@c/Layout'
-import FlexyRow from '@c/FlexyRow'
-import { Title } from '@c/Title'
-import { getShowBySlug } from '@l/graphcms'
-import { formatUSD, formatDate } from '@l/utils'
+import ReactMarkdown from "react-markdown";
+import styled from "styled-components";
+import Layout from "@c/Layout";
+import FlexyRow from "@c/FlexyRow";
+import { Title } from "@c/Title";
+import { getShowBySlug } from "@l/graphcms";
+import { formatUSD, formatDate } from "@l/utils";
 
 const Markdown = styled(ReactMarkdown)`
   img {
@@ -12,11 +12,11 @@ const Markdown = styled(ReactMarkdown)`
     border-radius: 20px;
     border: 4px solid currentColor;
   }
-`
+`;
 
 const ArtistName = styled.h2`
   text-align: center;
-`
+`;
 
 const ArtistPhoto = styled.div`
   background-image: url(${(p) => p.imageUrl});
@@ -27,21 +27,23 @@ const ArtistPhoto = styled.div`
   border-radius: 100px;
   border: 4px solid currentColor;
   margin: 0 auto;
-`
+`;
 
 const Portrait = ({ images = [] }) => {
   if (images.length > 0) {
-    const img = images[0]
-    return (
-      <ArtistPhoto imageUrl={img.url} />
-    )
+    const img = images[0];
+    return <ArtistPhoto imageUrl={img.url} />;
   }
-  return null
-}
+  return null;
+};
 
 export default function Shows({ show }) {
   return (
-    <Layout title={`${show.title} / next-graphcms-shows`} maxWidth="900px" padding="0 2em">
+    <Layout
+      title={`${show.title} / next-graphcms-shows`}
+      maxWidth="900px"
+      padding="0 2em"
+    >
       <Title>{show.title}</Title>
 
       <FlexyRow>
@@ -51,31 +53,45 @@ export default function Shows({ show }) {
 
       <Markdown source={show.description} />
 
-      {show.artists.map(artist => (
+      {show.artists.map((artist) => (
         <div key={artist.id}>
           <ArtistName>{artist.fullName}</ArtistName>
 
           <Portrait images={artist.images} />
 
           <FlexyRow justify="flex-start">
-            <a href={artist.webUrl} target="_blank">Website</a>
-            <a href={artist.facebookUrl} target="_blank">Facebook</a>
-            <a href={artist.instagramUrl} target="_blank">Instagram</a>
-            <a href={artist.youTubeUrl} target="_blank">YouTube</a>
+            <a href={artist.webUrl} target="_blank">
+              Website
+            </a>
+            <a href={artist.facebookUrl} target="_blank">
+              Facebook
+            </a>
+            <a href={artist.instagramUrl} target="_blank">
+              Instagram
+            </a>
+            <a href={artist.youTubeUrl} target="_blank">
+              YouTube
+            </a>
           </FlexyRow>
 
           <Markdown source={artist.bio} />
         </div>
       ))}
     </Layout>
-  )
+  );
 }
 
 export async function getServerSideProps({ params }) {
-  const { slug } = params
-  const show = (await getShowBySlug(slug))
+  const { slug } = params;
+  const show = await getShowBySlug(slug);
 
-  return {
-    props: { show },
+  if (show)
+    return {
+      props: { show },
+    };
+  else {
+    return {
+      notFound: true,
+    };
   }
 }
